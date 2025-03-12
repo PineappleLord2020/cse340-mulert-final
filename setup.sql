b2bfe310877b78c2c5a68d1f30575be238f1de93
PineappleLord2020 <mul20002@byui.edu>
2025-03-12 16:42:23 -0600

    A bit more work on the accounts end

--- setup.sql
+++ setup.sql
@@ -5,10 +5,10 @@ CREATE TABLE IF NOT EXISTS category (
 );
  
 -- Create the games table if it doesn't exist
-CREATE TABLE IF NOT EXISTS games (
-    game_id SERIAL PRIMARY KEY,
-    game_name TEXT NOT NULL,
-    game_description TEXT NOT NULL,
+CREATE TABLE IF NOT EXISTS cars (
+    car_id SERIAL PRIMARY KEY,
+    car_name TEXT NOT NULL,
+    car_description TEXT NOT NULL,
     category_id INTEGER NOT NULL,
     image_path TEXT NOT NULL,
     FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE
@@ -24,15 +24,15 @@ ON CONFLICT (category_id) DO NOTHING;
 SELECT setval('category_category_id_seq', (SELECT MAX(category_id) FROM category));
  
 -- Insert initial games
-INSERT INTO games (game_id, game_name, game_description, category_id, image_path) VALUES
-(1, 'Catan', 'A popular resource-trading and city-building game.', 1, '/images/games/catan.jpg'),
-(2, 'Risk', 'A world domination game of strategy and conquest.', 1, '/images/games/risk.jpg'),
-(3, 'Uno', 'A fast-paced card game of matching colors and numbers.', 2, '/images/games/uno.jpg'),
-(4, 'Apples to Apples', 'A fun word association game perfect for family and friends.', 2, '/images/games/apples-to-apples.jpg')
+INSERT INTO cars (car_id, car_name, car_description, category_id, image_path) VALUES
+(1, 'Catan', 'A popular resource-trading and city-building game.', 1, '/images/cars/catan.jpg'),
+(2, 'Risk', 'A world domination game of strategy and conquest.', 1, '/images/cars/risk.jpg'),
+(3, 'Uno', 'A fast-paced card game of matching colors and numbers.', 2, '/cars/games/uno.jpg'),
+(4, 'Apples to Apples', 'A fun word association game perfect for family and friends.', 2, '/images/cars/apples-to-apples.jpg')
 ON CONFLICT (game_id) DO NOTHING;
 
 -- Reset the games sequence
-SELECT setval('games_game_id_seq', (SELECT MAX(game_id) FROM games));
+SELECT setval('cars_car_id_seq', (SELECT MAX(car_id) FROM cars));
 
 -- Create the users table if it doesn't exist
 CREATE TABLE IF NOT EXISTS users (
---
+++ src/routes/account/index.js
@@ -0,0 +1,98 @@
+import { Router } from 'express';
+
+const registrationValidation = [
+    body("email")
+        .isEmail()
+        .withMessage("Invalid email format."),
+    body("password")
+        .matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/)
+        .withMessage("Password must be at least 8 characters long, include one uppercase letter, one number, and one symbol.")
+];
+
+const router = Router();
+
+router.get('/register', async(req, res) => {
+    res.locals.scripts.push("<script src='/js/registration.js'></script>");
+    res.render('account/register', { title: 'Register' });
+});
+
+
+router.post('/register', registrationValidation, async(req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPW = req.body.confirm_password;
+    const register = await registerUser;
+
+    const results = validationResult(req);
+    if (results.errors.length > 0) {
+        results.errors.forEach((error) => {
+            req.flash('error', error.msg);
+        });
+        res.redirect('/account/register');
+        return;
+    }
+
+    if (!email || !password || !confirmPW) {
+        req.flash('error', 'One or more fields left blank. Please fill all fields.')
+        res.redirect('/register');
+    }
+
+    if (email.length < 1) {
+        res.redirect('/register');
+        req.flash('error','Email was left blank.');
+        return;
+    }
+
+    if (password.length < 8) {
+        res.redirect('/register');
+        req.flash('error', "Password isn't long enough.")
+        return;
+    }
+
+    if (!confirmPW || password === confirmPW ) {
+        res.redirect('/register');
+        req.flash('error', "Passwords don't match. Please try again.")
+        return;
+    }
+
+    register(email, password);
+});
+
+
+router.get('login', async(req, res) => {
+    res.render('account/login', {title: 'Login'});
+});
+
+
+router.post('/login', async(req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const verify = await verifyUser;
+
+    if (!email || !password || !confirmPW) {
+        req.flash('error', 'One or more fields left blank. Please fill all fields.')
+        res.redirect('/register');
+    }
+
+    if (email.length < 1) {
+        res.redirect('/register');
+        req.flash('error','Email was left blank.');
+        return;
+    }
+
+    if (password.length < 8) {
+        res.redirect('/register');
+        req.flash('error', "Password isn't long enough.")
+        return;
+    }
+
+    verify(email, password);
+});
+
+
+router.get('/', requireAuth, (req, res) => {
+    res.render('account/index', {title: 'Account'});
+});
+
+
+export default router;
