7786396a049c1589d48e09002ee97d2d1bd5569a
PineappleLord2020 <mul20002@byui.edu>
2025-03-24 16:18:49 -0600

    Got my database synced up and working with my catalog

---
+++ src/models/vehicles/index.js
@@ -0,0 +1,17 @@
+import dbClient from "../index.js";
+
+const getAllVehicles = async () => {
+    const db = await dbClient;
+    const sql = 'SELECT * FROM vehicles';
+    const result = await db.query(sql);
+    return result.rows;
+};
+
+const getClassVehicles = async(vehicleClass) => {
+    const db = await dbClient;
+    const sql = "SELECT * FROM vehicles WHERE category_id = $1";
+    const result = await db.query(sql, [vehicleClass]);
+    return result.rows;
+}
+
+export {getAllVehicles, getClassVehicles};
--- src/routes/vehicles/index.js
+++ src/routes/vehicles/index.js
@@ -1,13 +1,22 @@
 import { Router } from 'express';
+import { getAllVehicles, getClassVehicles } from '../../models/vehicles/index.js';
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-    res.render('vehicles/index', { title: 'Vehicles' });
+    const vehicles = await getAllVehicles();
+    console.log(vehicles);
+    res.render('vehicles/index', { title: 'Vehicles', vehicles });
 });
 
 router.post('/', async (req, res) => {
     
+});
+
+router.get('/:id', async (req, res) => {
+    const vehicles = await getClassVehicles(req.params.id);
+    console.log(vehicles);
+    res.render('vehicles/index', { title: 'Vehicles', vehicles });
 });
 
 export default router;
